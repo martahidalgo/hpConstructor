02f7b1b0b9a9b50e514780478395acaf04927d74
Marta R. Hidalgo <marta.hidalgo@outlook.es>
2019-02-22 14:36:03 +0100

    Adapting names of RDatas to include version number

--- RDatas_constructor/pack_data_by_species.R
+++ RDatas_constructor/pack_data_by_species.R
@@ -6,29 +6,35 @@
 
 species <- c("hsa", "mmu", "rno")
 dbs <- c("GO", "uniprot")
-edpath <- "RDatas_constructor/RDatas/"
+setwd("RDatas_constructor/")
+edpath <- "RDatas/"
+version <- "v2"
 
 # XREF
 #------------------------
 for(spe in species){
-    xref_folder <- paste0("RDatas_constructor/xref/", spe, "/")
+    xref_folder <- paste0("xref/", spe, "/")
     xref_file <- paste0(xref_folder, "xref_", spe, ".RData")
     xr <- load(xref_file)
     xref <- get(xr)
-    save(xref, file=paste0(edpath, "xref_", spe, ".rda"), compress = "bzip2")
+    xref_file <- paste0(edpath, "xref_", spe, "_", version, ".rda")
+    save(xref, file = xref_file, compress = "bzip2")
 }
 
 
 # ANNOTATIONS
 #------------------------
-load("RDatas_constructor/annotations/annotations/go_bp_frame.RData")
-save(go_bp_frame, file = paste0(edpath, "go_bp_frame.rda"), compress = "bzip2")
-load("RDatas_constructor/annotations/annotations/go_bp_net.RData")
-save(go_bp_net, file = paste0(edpath, "go_bp_net.rda"), compress = "bzip2")
+load("annotations/annotations/go_bp_frame.RData")
+goframe_file <- paste0(edpath, "go_bp_frame_", version, ".rda")
+save(go_bp_frame, file = goframe_file, compress = "bzip2")
+
+load("annotations/annotations/go_bp_net.RData")
+gonet_file <- paste0(edpath, "go_bp_net_", version, ".rda")
+save(go_bp_net, file = gonet_file, compress = "bzip2")
 
 # GO_bp_annots
 for(spe in species){
-    gba_folder <- paste0("RDatas_constructor/annotations/annotations/", spe, "/")
+    gba_folder <- paste0("annotations/annotations/", spe, "/")
     gba_file <- paste0(gba_folder, "/go_bp_", spe, ".annot")
     go_bp_annot <- read.delim(gba_file,
                               sep = "\t",
@@ -37,14 +43,14 @@ for(spe in species){
                               comment.char = "!")
     go_bp_annot <- go_bp_annot[,1:3]
     colnames(go_bp_annot) <- c("gene", "function", "term")
-    bpfile <- paste0(edpath, "annot_GO_", spe, ".rda")
+    bpfile <- paste0(edpath, "annot_GO_", spe, "_", version, ".rda")
     save(go_bp_annot, file = bpfile, compress = "bzip2")
 }
 
 
 # Uni_bp_annots
 for(spe in species){
-    gba_folder <- paste0("RDatas_constructor/annotations/annotations/", spe, "/")
+    gba_folder <- paste0("annotations/annotations/", spe, "/")
     uba_file <- paste0(gba_folder, "/uniprot_keywords_",
                        spe, "__biological_process.annot")
     uni_bp_annot <- read.delim(uba_file,
@@ -54,20 +60,20 @@ for(spe in species){
                               comment.char = "!")
     uni_bp_annot <- uni_bp_annot[,1:2]
     colnames(uni_bp_annot) <- c("gene","function")
-    ubpfile <- paste0(edpath, "annot_uniprot_", spe, ".rda")
+    ubpfile <- paste0(edpath, "annot_uniprot_", spe, "_", version, ".rda")
     save(uni_bp_annot, file = ubpfile, compress = "bzip2")
 }
 
 # Entrez_HGNC
 for(spe in species){
-    eh_folder <- paste0("RDatas_constructor/annotations/annotations/", spe, "/")
+    eh_folder <- paste0("annotations/annotations/", spe, "/")
     eh_file <- paste0(eh_folder, "/entrez_hgnc_", spe, ".annot")
     entrez_hgnc <- read.delim(eh_file,
                               sep = "\t",
                               header = FALSE,
                               stringsAsFactors = FALSE,
                               comment.char = "!")
-    ehfile <- paste0(edpath, "entrez_hgnc_", spe, ".rda")
+    ehfile <- paste0(edpath, "entrez_hgnc_", spe, "_", version, ".rda")
     save(entrez_hgnc, file = ehfile, compress = "bzip2")
 }
 
@@ -77,38 +83,14 @@ for(spe in species){
 #------------------------
 # All together in different files
 for(spe in species){
-    path <- paste0("RDatas_constructor/pathways/", spe, "/temp/meta_graph_info_",
+    path <- paste0("pathways/", spe, "/temp/meta_graph_info_",
                    spe, ".RData")
     mgi <- load(path)
     meta_graph_info <- get(mgi)
     meta_graph_info$species <- spe
-    mgifile <- paste0(edpath, "meta_graph_info_", spe, ".rda")
+    mgifile <- paste0(edpath, "meta_graph_info_", spe, "_", version, ".rda")
     save(meta_graph_info, file = mgifile, compress = "bzip2")
 }
-
-
-# # Separatedly
-# # HSA
-# mgi <- load("RDatas_constructor/pathways/hsa/temp/meta_graph_info_hsa.RData")
-# meta_graph_info_hsa <- get(mgi)
-# meta_graph_info_hsa$species <- "hsa"
-# save(meta_graph_info_hsa, pkg = ".")
-# rm(metaginfo, meta_graph_info_hsa, mgi)
-# 
-# # RNO
-# mgi <- load("RDatas_constructor/pathways/rno/temp/meta_graph_info_rno.RData")
-# meta_graph_info_rno <- get(mgi)
-# meta_graph_info_rno$species <- "rno"
-# save(meta_graph_info_rno, pkg = ".")
-# rm(metaginfo, meta_graph_info_rno, mgi)
-# 
-# # MMU
-# mgi <- load("RDatas_constructor/pathways/mmu/temp/meta_graph_info_mmu.RData")
-# meta_graph_info_mmu <- get(mgi)
-# meta_graph_info_mmu$species <- "mmu"
-# save(meta_graph_info_mmu, pkg = ".")
-# rm(metaginfo, meta_graph_info_mmu, mgi)
-
 
 
 
@@ -116,9 +98,9 @@ for(spe in species){
 #------------------------
 for(db in dbs){
     for(spe in species){
-        load(paste0("RDatas_constructor/annofuns/annofuns_", db, "_", spe, ".RData"))
-        save(annofuns, file = paste0(edpath, "annofuns_", db, "_", spe, ".rda"), 
-             compress = "bzip2")
+        load(paste0("annofuns/annofuns_", db, "_", spe, ".RData"))
+        af <- paste0(edpath, "annofuns_", db, "_", spe, "_", version, ".rda")
+        save(annofuns, file = af, compress = "bzip2")
     }
 }
 
@@ -129,61 +111,12 @@ for(db in dbs){
 feats <- c("genes", "uniprot", "GO")
 for(spe in species){
     for(feat in feats){
-        file <- paste0("RDatas_constructor/pathways/pseudo/pmgi_", spe, "_", feat,".RData")
+        file <- paste0("pathways/pseudo/pmgi_", spe, "_", feat,".RData")
         pseudo <- load(file)
         pmgi <- get(pseudo)
-        save(pmgi, file = paste0(edpath, "pmgi_", spe, "_", feat, ".rda"), 
-             compress = "bzip2")
+        pf <- paste0(edpath, "pmgi_", spe, "_", feat, "_", version, ".rda")
+        save(pmgi, file = pf, compress = "bzip2")
         rm(list = pseudo)
     }
 }
 
-# # HSA
-# pmgi <- load("RDatas_constructor/pathways/pseudo/pmgi_hsa_genes.RData")
-# pmgi_hsa_genes <- get(pmgi)
-# save(pmgi_hsa_genes, pkg = ".")
-# rm(pmgi_hsa_genes, pmgi)
-# 
-# pmgi <- load("RDatas_constructor/pathways/pseudo/pmgi_hsa_uniprot.RData")
-# pmgi_hsa_uniprot <- get(pmgi)
-# save(pmgi_hsa_uniprot, pkg = ".")
-# rm(pmgi_hsa_uniprot, pmgi)
-# 
-# pmgi <- load("RDatas_constructor/pathways/pseudo/pmgi_hsa_GO.RData")
-# pmgi_hsa_GO <- get(pmgi)
-# save(pmgi_hsa_GO, pkg = ".")
-# rm(pmgi_hsa_GO, pmgi)
-# 
-# # RNO
-# pmgi <- load("RDatas_constructor/pathways/pseudo/pmgi_rno_genes.RData")
-# pmgi_rno_genes <- get(pmgi)
-# save(pmgi_rno_genes, pkg = ".")
-# rm(pmgi_rno_genes, pmgi)
-# 
-# pmgi <- load("RDatas_constructor/pathways/pseudo/pmgi_rno_uniprot.RData")
-# pmgi_rno_uniprot <- get(pmgi)
-# save(pmgi_rno_uniprot, pkg = ".")
-# rm(pmgi_rno_uniprot, pmgi)
-# 
-# pmgi <- load("RDatas_constructor/pathways/pseudo/pmgi_rno_GO.RData")
-# pmgi_rno_GO <- get(pmgi)
-# save(pmgi_rno_GO, pkg = ".")
-# rm(pmgi_rno_GO, pmgi)
-# 
-# # MMU
-# pmgi <- load("RDatas_constructor/pathways/pseudo/pmgi_mmu_genes.RData")
-# pmgi_mmu_genes <- get(pmgi)
-# save(pmgi_mmu_genes, pkg = ".")
-# rm(pmgi_mmu_genes, pmgi)
-# 
-# pmgi <- load("RDatas_constructor/pathways/pseudo/pmgi_mmu_uniprot.RData")
-# pmgi_mmu_uniprot <- get(pmgi)
-# save(pmgi_mmu_uniprot, pkg = ".")
-# rm(pmgi_mmu_uniprot, pmgi)
-# 
-# pmgi <- load("RDatas_constructor/pathways/pseudo/pmgi_mmu_GO.RData")
-# pmgi_mmu_GO <- get(pmgi)
-# save(pmgi_mmu_GO, pkg = ".")
-# rm(pmgi_mmu_GO, pmgi)
-# 
-# 
